54a0d7efc11f2f62810cbba98f4314c10255f7ec
DESKTOP-BH90N68\bctone <rancray@naver.com>
2023-07-06 21:21:29 +0900

    remade

--- main/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Product, Article, Tag
-
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Article)
-admin.site.register(Tag)
